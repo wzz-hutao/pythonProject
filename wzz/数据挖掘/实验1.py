cba4c6c760e97bad15f65c594d9f187627ce0dfa
wzzyyds <2890013545@qq.com>
2023-03-24 09:44:22 +0800

    Changes

--- wzz/数据挖掘/实验1.py
+++ wzz/数据挖掘/实验1.py
@@ -14,8 +14,8 @@ import numpy as np
 # print(df['Age'].mean())
 
 
-data = pd.read_csv("D:/资料/数据挖掘/titanic_train.csv")
-data = data[['Age','Fare']]
-plt.figure()
-data.boxplot(sym="r*",vert=False,patch_artist=True,meanline=False,showmeans=True)
-plt.show()
+# data = pd.read_csv("D:/资料/数据挖掘/titanic_train.csv")
+# data = data[['Age','Fare']]
+# plt.figure()
+# data.boxplot(sym="r*",vert=False,patch_artist=True,meanline=False,showmeans=True)
+# plt.show()
