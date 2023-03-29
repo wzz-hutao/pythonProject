2bfaee2c7602e7aa80b3df3bde3928d688e80bca
wzzyyds <2890013545@qq.com>
2023-03-29 15:26:37 +0800

    Changes

--- wzz/爬虫/2023.py
+++ wzz/爬虫/2023.py
@@ -1,4 +1,9 @@
 from urllib.request import urlopen
+import csv
+import re
+import requests
+import json
+
 
 url = 'http://www.baidu.com'
 resp = urlopen(url)
@@ -26,11 +31,8 @@ print("over")
 
 # pip清华源
 # pip install -i https://pypi.tuna.tsinghua.edu.cn/simple + (例子requests)
-import csv
-import re
 
-import requests
-import json
+
 
 # 爬取搜狗首页的页面数据
 # 指定url
@@ -202,3 +204,106 @@ for href in child_url_list:
     results3 = obj3.search(child_resp.text)
     print(results3.group("movie"))
     print(results3.group("download"))
+
+
+
+# bs4解析-html语法
+# <标签 属性="属性值">被标记的内容</标签>
+# <a href="http://www.baidu.com">周杰伦</a>
+# 例外：<img src="xxx.jpg"/> 图片        <br />
+
+
+
+# 示例，这个地址已经不存在了
+# 获取页面源代码
+# 使用bs4进行解析，拿到数据
+import requests
+from bs4 import BeautifulSoup
+import csv
+
+url = "http://www.xinfadi.com.cn/marketanalysis/0/list/1.shtml"
+resp = requests.get(url)
+# print(resp.text)
+f = open("北京新发地菜价.csv",mode="w")
+csvwriter = csv.writer(f)
+
+# 解析数据
+# 把页面源代码交给BeautifulSoup进行处理，生产bs对象
+# html.parser 指定html解析器
+page = BeautifulSoup(resp.text,"html.parser")
+# 从bs对象中查找数据
+# find(标签名，属性=属性值)：找一个数据返回
+# find_all(标签名，属性=属性值)：找一堆数据返回
+# class是python的一个关键字，直接写会报错
+# table = page.find("table",class_="hq_table")
+# 和上一行是一个意思，这样书写可以避免class
+table = page.find("table",attrs={"class":"hq_table"})
+# print(table)
+# 拿到所有的数据
+trs = table.find_all("tr")[1:]
+for tr in trs:
+  tds = tr.find_all("td")
+  name = tds[0].text #.text 表示拿到被标签记的内容
+  low = tds[1].text #.text 表示拿到被标签记的内容
+  avg = tds[2].text #.text 表示拿到被标签记的内容
+  high = tds[3].text #.text 表示拿到被标签记的内容
+  gui = tds[4].text #.text 表示拿到被标签记的内容
+  kind = tds[5].text #.text 表示拿到被标签记的内容
+  date = tds[6].text #.text 表示拿到被标签记的内容
+  print(name,low,avg,high,gui,kind,date)
+  csvwriter.writerow([name,low,avg,high,gui,kind,date])
+
+f.close()
+
+
+
+# 新地址处理
+import requests
+import json
+import jsonpath
+from concurrent.futures import ThreadPoolExecutor
+
+print("名称\t最高价\t平均价\t最低价")
+f = open("D:/python爬虫/北京新发地菜价.txt", "w")
+
+
+def main(current):
+    url = "http://www.xinfadi.com.cn/getPriceData.html"
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36"}
+    data = {"limit": "20",
+            "current": f"{current + 1}",
+            "pubDateStartTime": "",
+            "pubDateEndTime": "",
+            "prodPcatid": "",
+            "prodCatid": "",
+            "prodName": ""}
+    parsePrint(url, headers, data)
+
+
+def parsePrint(url, headers, data):
+    res = requests.post(url, headers=headers, data=data)
+    jsonbj = json.loads(res.text)
+    # 解析数据
+    name = jsonpath.jsonpath(jsonbj, "$..prodName")
+    highPrice = jsonpath.jsonpath(jsonbj, "$..highPrice")
+    avgPrice = jsonpath.jsonpath(jsonbj, "$..avgPrice")
+    lowPrice = jsonpath.jsonpath(jsonbj, "$..lowPrice")
+    # 打印信息
+    for g in range(0, 20):
+        # 输出数据
+        print(f"{name[g]}\t{highPrice[g]}\t{avgPrice[g]}\t{lowPrice[g]}\n")
+        # 写入数据
+        f.write(f"{name[g]}\t{highPrice[g]}\t{avgPrice[g]}\t{lowPrice[g]}\n")
+
+
+if __name__ == '__main__':
+    # 创立线程池
+    with ThreadPoolExecutor(2) as t:
+        for i in range(2):  # 爬取页数
+            # 给子线程提交任务并执行
+            t.submit(main, current=i)
+    print("下载完成")
+    f.close()
+
+
